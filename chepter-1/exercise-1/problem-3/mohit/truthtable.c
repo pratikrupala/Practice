c18f329ae77b24bd858f81d6669030ce18dc64c6
root <root@mohitarya.localdomain>
2014-12-25 21:01:52 +0530

    Added code for genration of the truth table using recursion

---
+++ chepter-1/exercise-1/problem-3/mohit/truthtable.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	int n, i=0, j=0;
+	int *arr;
+	printf("Inpue the number of boolean variables\n");
+	scanf("%d", &n);
+	arr = (int *) malloc(sizeof(int) * n);
+	for(j; j < n; j++) {
+		arr[j] = 0;
+	}
+	truth_table(arr, n-1, i); 
+	return 0;
+}
+
+truth_table(int *arr, int n, int i)
+{
+	int j, k=1, l;
+	if (i == n) {
+		while(k <= 2) {
+			for (l=0; l <= n; l++) {
+				printf("%d", arr[l]);
+			}
+			printf("\n");
+			arr[n] = 1;
+			k++;
+		}
+	}
+	else {
+		for (j=0; j < 2; j++) {
+			truth_table(arr, n, i+1);
+			arr[i] = 1;
+			for (l=i+1; l <=n; l++) {
+				arr[l] = 0;
+			}
+		}
+	}
+}
