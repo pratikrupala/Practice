c95f611915e949eb461cbb8cd759aa0d5dd202d4
mohitarya <mohitarya8@gmail.com>
2014-12-31 06:53:51 -0500

    Added code for print in ascending order for x,y,z

---
+++ chepter-1/exercise-1/problem-4/mohit/ascending.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+#define MIN(a,b) (a==b)?a:(a<b)?a:b
+
+int main()
+{
+	int x,y,z,min, inter_min;
+	printf("Input the first No.\n");
+	scanf("%d",&x);
+	printf("Input the Second No.\n");
+	scanf("%d",&y);
+	printf("Input the Third No.\n");
+	scanf("%d",&z);
+	inter_min = MIN(x,y);
+	min = MIN(inter_min, z);
+	printf("Min = %d\n", min);			
+	if(x == min) {
+		if (y <= z)
+			printf("%d %d %d\n", x, y, z);	
+		else
+			printf("%d %d %d\n", x, z, y);
+	}
+	else if (y == min) {
+		if (x <= z)
+			printf("%d %d %d\n", y, x, z);
+		else
+			printf("%d %d %d\n", y, z, x);
+	}
+	else
+		if (x <= y)
+			printf("%d %d %d\n", z, x, y);
+		else
+			printf("%d %d %d\n", z, y, x);
+	return 0;
+}
