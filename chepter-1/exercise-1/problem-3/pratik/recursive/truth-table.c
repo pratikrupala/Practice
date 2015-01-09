e8d3961ce259df1287d795028e757aab7d7993c9
Pratik Rupala <prtrpl@gmail.com>
2015-01-09 13:53:33 +0530

    Added a program of truth table with recursive method with better algorithm

---
+++ chepter-1/exercise-1/problem-3/pratik/recursive/truth-table.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define SWAP(a, b, c) ((c) = (a), (a) = (b), (b) = (c))
+
+void table(int n, char *s, int i)
+{
+	int j = 0;
+	char c;
+	char z = '0', o = '1';
+
+	if(n == 0) {
+		for(j = n; j <= i; j++) {
+			printf("%c", *(s + j));
+		}
+		printf("\n");
+		return;
+	}
+	
+	SWAP(*(s+i), z, c);
+	table(n - 1, s, i + 1);
+	SWAP(*(s+i), o, c);
+	table(n - 1, s, i + 1);
+}
+
+int main(void)
+{
+	char *s;
+	int n = 0, i;
+
+	printf("Enter the range of table:");
+	scanf("%d", &n);
+
+	s = (char *) malloc(n);
+
+	table(n, s, 0);
+
+	return 0;
+}
