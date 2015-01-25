99a4611fa307ed046a6bd5900aca45ed14bfe958
root <root@mohitarya.localdomain>
2015-01-25 20:11:52 +0530

    updated tower of hanoi code but still incomplete

--- chepter-1/exercise-1/problem-11/mohit/tower_of_hanoi.c
+++ chepter-1/exercise-1/problem-11/mohit/tower_of_hanoi.c
@@ -15,12 +15,44 @@
 
 int main()
 {
-	int disks;
+	int disks, curr_tower=1, targ_tower=3, other_tower=2;
 	printf("Input the number of disk in the first tower\n");
+	scanf("%d", &disks);
 	if (disks < 3) {
 		printf("Number of disks must be greater then 03\n");
 		printf("Please enter a valid number of disks\n");
 		exit(0);
 	}
+	toh(disks, curr_tower, targ_tower, other_tower);
+}
+
+toh(int disks, int curr_tower, int targ_tower, int other_tower)
+{
+	if(disks == 3)
+	{
+		move_targ_tower(curr_tower, targ_tower, other_tower);
+	}
+	else {
+		toh(disks-1, curr_tower, other_tower, targ_tower);
+		if(disks-1 == 3) {
+			printf("Put disk no %d to %d tower\n", disks, targ_tower);
+			toh(disks-1, other_tower, targ_tower, curr_tower);	
+		}
+		printf("Put disk no %d to %d tower\n", disks, other_tower);
+		if (disk-1 >= 3) {
+			toh(disk-1, );
+		}
+	}
+}
+
+move_targ_tower(int curr_tower, int targ_tower, int other_tower)
+{
+	printf("Put disk no %d to %d tower\n", 1, targ_tower);
+	printf("Put disk no %d to %d tower\n", 2, other_tower);
+	printf("Put disk no %d to %d tower\n", 1, other_tower);
+	printf("Put disk no %d to %d tower\n", 3, targ_tower);
+	printf("Put disk no %d to %d tower\n", 1, curr_tower);
+	printf("Put disk no %d to %d tower\n", 2, targ_tower);
+	printf("Put disk no %d to %d tower\n", 1, targ_tower);
 	
 }
